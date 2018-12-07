c070dcfcf22f5e7a7cfa4b8f65928ff531a99588
Hubert Łępicki <hubert.lepicki@amberbit.com>
2018-12-07 11:03:16 +0100

    Add episode #2

--- polish-up-your-javascript-2/src/index.js
+++ polish-up-your-javascript-2/src/index.js
@@ -1,4 +1,56 @@
 import "./index.scss"
+import ReactDOM from "react-dom";
+import React from "react";
 
-(() => alert("Hello, ES6 world!"))()
+const reactRoot = document.createElement("div");
+reactRoot.setAttribute("id", "react-root");
+document.body.appendChild(reactRoot);
+
+// Initial "Hello world!"
+//const App = (props) => <div>Hello, React!</div>;
+
+// this is equivallent to writing a traditional function:
+/*
+function App(props) {
+  return(<div>Hello, React!</div>);
+};
+*/
+
+// or class-based component example:
+/*
+class App extends React.Component {
+  render() {
+    return <div>Hello, class-based React!</div>;
+  }
+}
+*/
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {id: 1, completed: false, description: "Write blog post"},
+        {id: 2, completed: false, description: "Publish blog post"},
+        {id: 3, completed: false, description: "Profit!"}
+      ]
+    }
+  }
+  render() {
+    return <Tasks tasks={this.state.tasks} />;
+  }
+}
+
+const Tasks = (props) =>
+  <ul>
+    { props.tasks.map((task) => <Task key={task.id} task={task} />) }
+  </ul>
+
+const Task = (props) =>
+  <li>
+    { props.task.description }
+  </li>
+
+
+ReactDOM.render(<App />, reactRoot);
 
